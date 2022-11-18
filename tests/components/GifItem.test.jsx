0e22f73d1635a2f1f447d25b5afaacf2def8b633
Ruben Rodriguez <rrodriguez@opentrends.net>
2022-11-18 10:36:16 +0100

    GifItem.jsx advanced tests

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('Prueba de <GifItem />', () => {
@@ -7,5 +7,18 @@ describe('Prueba de <GifItem />', () => {
     test('Validación del snapshot', () => {
         const { container } = render(<GifItem title={title} url={url} />);
         expect(container).toMatchSnapshot();
+    });
+
+    test('Debe mostrar la imagen con la URL y el ALT indicado', () => {
+        render(<GifItem title={title} url={url} />);
+
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('Debe mostrar el títutlo en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title).innerHTML).toBeTruthy();
     });
 });
